03cf46dfb44cf59e5ff7ffb9a78d00d0de66df36
irawan19 <irawan_agungnugroho@yahoo.com>
2022-07-13 14:18:38 +0700

    update

--- docs/docs.go
+++ docs/docs.go
@@ -235,7 +235,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "2.0",
-	Host:             "localhost:8080",
+	Host:             "api.yeah.biz.id",
 	BasePath:         "/api/v2",
 	Schemes:          []string{},
 	Title:            "API Yeah",
