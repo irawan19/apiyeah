c4346a233da906ac827e1575b6476b98db3b0e29
irawan19 <irawan_agungnugroho@yahoo.com>
2022-07-13 14:14:11 +0700

    update

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"apiyeah/router"
-	"log"
+	"apiyeah/controller"
 	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 // @title API Yeah
@@ -13,7 +17,23 @@ import (
 // @host api.yeah.biz.id
 // @BasePath /api/v2
 func main() {
-	r := router.Router()
+	router := mux.NewRouter()
 
-	log.Println(http.ListenAndServe(":8080", r))
+	//Data
+	router.HandleFunc("/api/v2/data/konfigurasiaplikasi", controller.AmbilKonfigurasiAplikasi).Methods("GET")
+	router.HandleFunc("/api/v2/data/jeniskelamin", controller.AmbilJenisKelamin).Methods("GET")
+	router.HandleFunc("/api/v2/data/pembayaran", controller.AmbilPembayaran).Methods("GET")
+	router.HandleFunc("/api/v2/data/statuspembayaran", controller.AmbilPembayaran).Methods("GET")
+
+	//Event
+	router.HandleFunc("/api/v2/event", controller.AmbilSemuaEvent).Methods("GET")
+	router.HandleFunc("/api/v2/event/detail/{id}", controller.AmbilDetailEvent).Methods("GET")
+	router.HandleFunc("/api/v2/event/cekticket/{bookingcode}", controller.CekTicket).Methods("POST")
+	router.HandleFunc("/api/v2/event/registrasi", controller.Registrasi).Methods("POST")
+	router.PathPrefix("/").Handler(httpSwagger.WrapHandler)
+
+	headersOk := handlers.AllowedHeaders([]string{"Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router))
 }
