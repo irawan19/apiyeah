923e6c8ed756d852953b7f98d1a7eb93eddf0b1e
irawan19 <irawan_agungnugroho@yahoo.com>
2022-07-13 11:48:46 +0700

    update

--- router/router.go
+++ router/router.go
@@ -16,16 +16,16 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	//Data
-	router.HandleFunc("/api/v2/data/konfigurasiaplikasi", controller.AmbilKonfigurasiAplikasi).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/v2/data/jeniskelamin", controller.AmbilJenisKelamin).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/v2/data/pembayaran", controller.AmbilPembayaran).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/v2/data/statuspembayaran", controller.AmbilPembayaran).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/v2/data/konfigurasiaplikasi", controller.AmbilKonfigurasiAplikasi).Methods("GET")
+	router.HandleFunc("/api/v2/data/jeniskelamin", controller.AmbilJenisKelamin).Methods("GET")
+	router.HandleFunc("/api/v2/data/pembayaran", controller.AmbilPembayaran).Methods("GET")
+	router.HandleFunc("/api/v2/data/statuspembayaran", controller.AmbilPembayaran).Methods("GET")
 
 	//Event
-	router.HandleFunc("/api/v2/event", controller.AmbilSemuaEvent).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/v2/event/detail/{id}", controller.AmbilDetailEvent).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/v2/event/cekticket/{bookingcode}", controller.CekTicket).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/v2/event/registrasi", controller.Registrasi).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/v2/event", controller.AmbilSemuaEvent).Methods("GET")
+	router.HandleFunc("/api/v2/event/detail/{id}", controller.AmbilDetailEvent).Methods("GET")
+	router.HandleFunc("/api/v2/event/cekticket/{bookingcode}", controller.CekTicket).Methods("POST")
+	router.HandleFunc("/api/v2/event/registrasi", controller.Registrasi).Methods("POST")
 	router.PathPrefix("/").Handler(httpSwagger.WrapHandler)
 	http.Handle("/", router)
 
