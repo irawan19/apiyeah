c46e31c01b9d0822c3affe6dc2738cd21e96cb0e
irawan19 <irawan_agungnugroho@yahoo.com>
2022-07-24 22:18:48 +0700

    update pembayaran

--- models/datamodels.go
+++ models/datamodels.go
@@ -150,7 +150,7 @@ func AmbilSemuaPembayaran() ([]Pembayaran, error) {
 						nama_pembayarans,
 						nama_rekening_pembayarans,
 						no_rekening_pembayarans,
-						logo_pembayarans,
+						logo_pembayarans
 					FROM master_pembayarans
 					JOIN master_tipe_pembayarans ON master_tipe_pembayarans.id_tipe_pembayarans=master_pembayarans.tipe_pembayarans_id
 					WHERE status_hapus_pembayarans=0`
